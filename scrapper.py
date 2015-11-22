261ca2e601b0bcea381a9d44cd78a4629f2ca58c
dustinchang <1022dustin@gmail.com>
2015-11-21 23:26:48 -0800

    cleanup & doesnt need json or regex

--- scrapper.py
+++ scrapper.py
@@ -1,22 +1,13 @@
 from googlefinance import getQuotes
-import json
-y = getQuotes('Googl')
-x = json.dumps(getQuotes('Googl'), indent=2)
-print type(x)
-print x
-print type(y)
-print y
-print y[0]
-print len(y)
-print y[0][u'LastTradePrice']
-
 
 def scrape(ticker):
     realtime_qoute = getQuotes(ticker)
     print realtime_qoute[0][u'LastTradePrice']
+    return realtime_qoute[0][u'LastTradePrice']
 
 def main():
-    scrape('googl')
+    s = scrape('googl')
+    print s
 
 if __name__ == "__main__":
     main()
