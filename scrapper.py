1698a79ab7115e2a6679dbff189fe6a2f2c2b260
dustinchang <dustinlchang@gmail.com>
2015-11-22 18:52:47 -0800

    comments

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,13 @@
+#Requires and runs on googlefinance
 from googlefinance import getQuotes
 
+#Call to get the real time quote for any given stock ticker
 def scrape(ticker):
     realtime_qoute = getQuotes(ticker)
     print realtime_qoute[0][u'LastTradePrice']
     return realtime_qoute[0][u'LastTradePrice']
 
+#Main for testing
 def main():
     s = scrape('googl')
     print s
