e73d126ff0ab016ea3a5a10cbb15d9e9cca4fa10
dustinchang <1022dustin@gmail.com>
2015-12-08 23:49:32 -0800

    getting avg_prices

--- analysis.py
+++ analysis.py
@@ -3,25 +3,51 @@ from main import Client
 from main import Investment
 from main import Stock
 
+def change(curr_price, prev_price, avg_prices):
+    avg_prices.append(round(float(curr_price)/float(prev_price), 2))
+
 ##Determine the patterns between highs and lows over
 # the past 2 week business days
 #
 def stock_patterns(stk_hist):
-    pass
+    close_prices = []
+    for stock in stk_hist:
+         close_prices.append(stock.StockClose)
+    print close_prices
+
+    prev_curr = close_prices[0]
+    curr = 0
+    avg_prices = []
+    for i, price  in enumerate(close_prices):
+        curr = price
+        if i % 9 == 0:
+            change(curr, prev_curr, avg_prices)
+            print "price:{} curr:{} prev:{}".format(price, curr, prev_curr)
+            prev_curr = price
+        else:
+            pass
+    print avg_prices
+
+        #curr = price
+        #if curr/prev_curr < .95:
+        #    pass
+
+
+
 
 ##Evaluate the past 2 weeks of a stocks trading prices
 # Determine the appropriate response for stock trader
 #
 def stock_estimate(portfolio, symb):
     stk_hist = get_historical(symb, 15)
+    stock_patterns(stk_hist)
     #determine the stock_patterns
     print stk_hist
-    print stk_hist[0].StockID
     print len(stk_hist)
     z = 1
-    for x in stk_hist:
-        print "{}: {}".format(z, x.StockID)
-        z+=1
+    #for x in stk_hist:
+    #    print "{}: {}".format(z, x.StockID)
+    #    z+=1
 
 
     for stock in portfolio:
@@ -43,7 +69,7 @@ def main():
     print client1.Portfolio[0].StockVolume
     print client1.Portfolio[0].PriceTraded
 
-    stock_estimate(client1.Portfolio, 'GOOGL')
+    stock_estimate(client1.Portfolio, 'AAPL')
 
 if __name__ == "__main__":
     main()
